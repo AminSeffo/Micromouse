8f52e259a0c3b8dcbd1276e0d337658ded0bbabe
Amin Seffo <92951154+AminSeffo@users.noreply.github.com>
2023-06-13 13:14:43 +0200

    Update adc.c

--- micromouse_exercises.X/adc.c
+++ micromouse_exercises.X/adc.c
@@ -25,7 +25,7 @@ void setupADC1()
     AD1CON1bits.ADON = 0; // disable ADC1 module
    
     
-    //AD1PCFGL = 0xffff;     //  0b1111 1111 1110 1100 AN0,1 and 4  analog inputs
+    //AD1PCFGL = 0xffff;     //  0b1111 1111 1110 1100 AN0,1 and 4  analog inputs; here you can address multiple channels  
     
     ADPCFGbits.PCFG5=0; // potentiometer is connected AN5 
     AD1CON1bits.ADSIDL=0;//no sampling in idle mode
