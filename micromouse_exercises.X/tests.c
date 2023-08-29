760682e1010f5968d8a5fad7bbcbfad45eba8dff
Yara Schattschneider <61124827+yarasc@users.noreply.github.com>
2023-08-29 19:48:19 +0200

    added a test

--- micromouse_exercises.X/tests.c
+++ micromouse_exercises.X/tests.c
@@ -13,3 +13,18 @@ void runLedTest()
 
     LED3 = LEDOFF;
 }
+
+
+void runEncoderTest(float pos){
+    setupIO();
+    LED1 = LEDOFF;
+    LED2 = LEDOFF;
+     
+    if (getPositionInCounts_1()>pos){
+        LED1 = LEDON;
+    }
+    if (getPositionInCounts_2()>pos){
+        LED2 = LEDON;
+    }
+    
+}
