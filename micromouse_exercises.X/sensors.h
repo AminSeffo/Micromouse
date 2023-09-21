ae390bf79795b4e6f6aced9bc0ee6885ad0cfce6
AminSeffo <amin.seffo@tum.de>
2023-09-21 18:06:18 +0200

    some defs

--- micromouse_exercises.X/sensors.h
+++ micromouse_exercises.X/sensors.h
@@ -14,12 +14,21 @@
 #define LEFT_SENSOR_DATA DISTANCE_SENSOR2
 #define RIGHT_SENSOR_DATA DISTANCE_SENSOR3
 
+#define BATTERY_SENSOR_DATA BATTERY_VOLTAGE
+
+#define BATTERY_DIVIDER_R1 20000.0 
+#define BATTERY_DIVIDER_R2 10000.0
+#define ADC_REFERENCE_VOLTAGE 3.3   // ADC reference voltage
+
 void initSensors();
 
 float convert_sensor_data_to_distance(int sensor_data);
 float get_left_distance_in_m(void);
 float get_right_distance_in_m(void);
 float get_front_distance_in_m(void);
+float getBatteryVoltage(int adcValue)
+
+
 
 
 #endif	/* SENSORS_H */
